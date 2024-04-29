473ab68a3e684a0e3630c47589df44b9c02212e5
Rushil <mahathorushil59@gmail.com>
2024-04-29 19:07:04 +0200

    fixed nav bar issue and created new timesheet page

--- app/src/main/java/za/co/varsitycollege/st10215473/rvmtimesolutions/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10215473/rvmtimesolutions/MainActivity.kt
@@ -1,7 +1,8 @@
 package za.co.varsitycollege.st10215473.rvmtimesolutions
 
+import android.graphics.Rect
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,53 +11,56 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavBar: BottomNavigationView
-
+    private lateinit var rootView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        rootView = findViewById(R.id.main) // Root view for layout listener
+        bottomNavBar = findViewById(R.id.bottom_nav)
+
+        setupWindowInsets()
+        setupBottomNavigation()
+        replaceFragment(DashboardFragment())
+
+        observeKeyboardVisibility()
+    }
+
+    private fun setupWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        bottomNavBar = findViewById(R.id.bottom_nav)
-        bottomNavBar.setItemIconTintList(null)
-        bottomNavBar.setOnItemSelectedListener { menuItem ->
-            when(menuItem.itemId)
-            {
-                R.id.bottom_dashboard -> {
-                        replaceFragment(DashboardFragment())
-                        true
-                }
-                R.id.bottom_calendar -> {
-                    replaceFragment(CalendarFragment())
-                    true
-                }
-                R.id.bottom_add -> {
-                    replaceFragment(AddFragment())
-                    true
-                }
-                R.id.bottom_timesheet -> {
-                    replaceFragment(TimesheetFragment())
-                    true
-                }
-                R.id.bottom_profile-> {
-                    replaceFragment(ProfileFragment())
-                    true
-                }
-                else -> false
-            }
-
-        }
-        replaceFragment(DashboardFragment())
     }
 
-    private fun replaceFragment(fragment: Fragment)
-    {
+    private fun setupBottomNavigation() {
+        bottomNavBar.setItemIconTintList(null)
+        bottomNavBar.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_dashboard -> replaceFragment(DashboardFragment())
+                R.id.bottom_calendar -> replaceFragment(CalendarFragment())
+                R.id.bottom_add -> replaceFragment(AddFragment())
+                R.id.bottom_timesheet -> replaceFragment(TimesheetFragment())
+                R.id.bottom_profile -> replaceFragment(ProfileFragment())
+                else -> false
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
+    private fun observeKeyboardVisibility() {
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            rootView.getWindowVisibleDisplayFrame(r)
+            val screenHeight = rootView.rootView.height
+            val keypadHeight = screenHeight - r.bottom
+
+            bottomNavBar.visibility = if (keypadHeight > screenHeight * 0.15) View.GONE else View.VISIBLE
+        }
     }
 }
